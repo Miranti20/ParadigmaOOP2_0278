5c3d42ace7ea825db92c4af55c662b37cef60c1d
Miranti20 <rantirarrr4505@gmail.com>
2025-06-03 11:15:30 +0700

    membuat class untuk baseclass

---
+++ noOverrading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class baseclass {
+    public:
+       virtual void perkenalan() {
+          cout << "hallo saya funtion dari base class";
+
+       }
+};
+
+class derivedclass : public baseclass {
+    public:
+       void perkenalan() {
+          cout << "hallo saya function dari base class";
+
+       }
+};
+
+int main() {
+    derivedclass a;
+    a.perkenalan();
+
+    return 0;
+}
